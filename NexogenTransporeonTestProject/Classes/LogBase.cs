300029295e120cbb4e7ae9a08891f66a9fa4d225
koltaicsaba <koltaicsabi@hotmail.com>
2023-03-10 07:54:54 +0100

    LogBase class added

---
+++ NexogenTransporeonTestProject/Classes/LogBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NexogenTransporeonTestProject.Classes
+{
+	public abstract class LogBase
+	{
+		public abstract void LogDebug(string message);
+		public abstract void LogInfo(string message);
+		public abstract void LogError(string message);
+	}
+}
+
