0904791ce90c3a4e0c7b3ae96ac00865da72df1b
koltaicsaba <koltaicsabi@hotmail.com>
2023-03-13 11:52:03 +0100

    FileLogger refactor

--- NexogenTransporeonTestProject/Classes/FileLogger.cs
+++ NexogenTransporeonTestProject/Classes/FileLogger.cs
@@ -6,13 +6,30 @@ namespace NexogenTransporeonTestProject.Classes
     public class FileLogger : LogBase
     {
         private readonly string _path;
+        private readonly string _filePath;
 
         public FileLogger(string path)
         {
             _path = path;
+            _filePath = _path + "log.txt";
         }
 
         public override void Log(LogLevel logLevel, string message)
+        {
+            RotateLogFiles();
+
+            if (File.Exists(_filePath))
+            {
+                File.ReadAllLines(_filePath);
+                File.AppendAllLines(_filePath, new List<string>() { GetFormattedMessage(logLevel, message) });
+            }
+            else
+            {
+                File.WriteAllLines(_filePath, new List<string>() { GetFormattedMessage(logLevel, message) });
+            } 
+        }
+
+        public void RotateLogFiles()
         {
             DirectoryInfo d = new DirectoryInfo(_path);
             FileInfo[] files = d.GetFiles("*.txt");
@@ -21,36 +38,22 @@ namespace NexogenTransporeonTestProject.Classes
             logFileNames = files.Where(x => x.Name.Contains("log")).Select(x => x.Name).ToList();
             logFileIndexes = logFileNames.Where(x => x.Split('.').Length > 2).Select(x => int.Parse(x.Split('.')[1])).ToList();
             int maxIndex = 0;
+            long length = 0;
 
             if (logFileIndexes.Count() > 0)
             {
                 maxIndex = logFileIndexes.Max();
             }
 
-            string filePath = _path + "log.txt";
-            long length = 0;
-            bool fileExists = File.Exists(filePath);
-
-            if (fileExists)
+            if (File.Exists(_filePath))
             {
-                length = new FileInfo(filePath).Length;
+                length = new FileInfo(_filePath).Length;
             }
 
             if (length > 5000)
             {
-                File.Move(filePath, _path + String.Format("log.{0}.txt", maxIndex + 1));
+                File.Move(_filePath, _path + String.Format("log.{0}.txt", maxIndex + 1));
             }
-
-            if (fileExists)
-            {
-                File.ReadAllLines(filePath);
-                File.AppendAllLines(filePath, new List<string>() { GetFormattedMessage(logLevel, message) });
-            }
-            else
-            {
-                File.WriteAllLines(filePath, new List<string>() { GetFormattedMessage(logLevel, message) });
-            }
-            
         }
     }
 }
