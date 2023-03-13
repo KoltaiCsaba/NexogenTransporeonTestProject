3c4a42e788573dc7c7215b0c36ea0c98b366ac59
koltaicsaba <koltaicsabi@hotmail.com>
2023-03-13 12:44:39 +0100

    Added logger testings to Program.cs

--- NexogenTransporeonTestProject/Program.cs
+++ NexogenTransporeonTestProject/Program.cs
@@ -1,3 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using NexogenTransporeonTestProject.Classes;
 
+Console.WriteLine("Testing the log library...");
+Console.WriteLine("Testing the console logger...");
+LogBase consoleLogger = new ConsoleLogger();
+consoleLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Debug, "This is a debug message.");
+consoleLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Info, "This is an info message.");
+consoleLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Error, "This is an error message.");
+Console.WriteLine("Success.");
+Console.Read();
+
+Console.WriteLine("Testing the file logger.");
+LogBase fileLogger = new FileLogger("/Users/koltaicsaba/Logs/");
+fileLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Debug, "This is a debug message.");
+fileLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Info, "This is an info message.");
+fileLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Error, "This is an error message.");
+Console.WriteLine("Success.");
+Console.Read();
+
+Console.WriteLine("Testing the stream logger.");
+MemoryStream streamLog = new MemoryStream();
+LogBase streamLogger = new StreamLogger(streamLog);
+streamLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Debug, "This is a debug message.");
+streamLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Info, "This is an info message.");
+streamLogger.Log(NexogenTransporeonTestProject.Enums.LogLevel.Error, "This is an error message.");
+
+using (StreamReader streamReader = new StreamReader(streamLog))
+{
+    streamLog.Position = 0;
+    string? line;
+
+    while ((line = streamReader.ReadLine()) != null)
+    {
+        Console.WriteLine(line);
+    }
+};
+
+Console.WriteLine("Success.");
+Console.Read();
